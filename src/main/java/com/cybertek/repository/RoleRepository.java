9b587b1f7124fc3f6b12a623307aa2e272d30eb5
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-15 14:08:11 +0100

    Ticketing Project - ORM

--- src/main/java/com/cybertek/repository/RoleRepository.java
+++ src/main/java/com/cybertek/repository/RoleRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-
+    Role findByDescription(String description);
 
 }
