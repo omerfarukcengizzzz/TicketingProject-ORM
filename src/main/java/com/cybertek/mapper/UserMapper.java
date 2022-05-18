7dd2cd1da493832e70402b250ceefab96b73b51e
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-18 15:57:34 +0100

    Ticketing Project - ORM

---
+++ src/main/java/com/cybertek/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.cybertek.mapper;
+
+import com.cybertek.dto.UserDTO;
+import com.cybertek.entity.User;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public User convertToEntity(UserDTO userDTO) {
+        return modelMapper.map(userDTO, User.class);
+    }
+
+    public UserDTO convertToDTO(User user) {
+        return modelMapper.map(user, UserDTO.class);
+    }
+
+}
