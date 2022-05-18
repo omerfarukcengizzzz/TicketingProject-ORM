5a6e9dfa1901b4662fd22e89aaf3606e1f99d62a
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-18 15:58:03 +0100

    Ticketing Project - ORM

--- src/main/java/com/cybertek/implementation/UserServiceImpl.java
+++ src/main/java/com/cybertek/implementation/UserServiceImpl.java
@@ -1,21 +1,39 @@
 package com.cybertek.implementation;
 
 import com.cybertek.dto.UserDTO;
+import com.cybertek.entity.User;
+import com.cybertek.mapper.UserMapper;
+import com.cybertek.repository.UserRepository;
 import com.cybertek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public List<UserDTO> listAllUsers() {
-        return null;
+        List<User> userList = userRepository.findAll(Sort.by("firstName"));
+
+        return userList.stream()
+                .map(obj -> userMapper.convertToDTO(obj))
+                .collect(Collectors.toList());
     }
 
     @Override
     public UserDTO findByUserName(String username) {
-        return null;
+        User user = userRepository.findByUserName(username);
+
+        return userMapper.convertToDTO(user);
     }
 
     @Override
