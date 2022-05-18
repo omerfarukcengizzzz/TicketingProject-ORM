6e3f736a7216f967688143255cc182549abed919
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-18 14:32:03 +0100

    Ticketing Project - ORM

--- src/main/java/com/cybertek/mapper/RoleMapper.java
+++ src/main/java/com/cybertek/mapper/RoleMapper.java
@@ -4,7 +4,9 @@ import com.cybertek.dto.RoleDTO;
 import com.cybertek.entity.Role;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RoleMapper {
 
     @Autowired
