cbcfc6cc87053eb0c561b88290f1d70e8909402d
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-19 16:41:26 +0100

    Ticketing Project - ORM

--- src/main/java/com/cybertek/service/UserService.java
+++ src/main/java/com/cybertek/service/UserService.java
@@ -7,9 +7,15 @@ import java.util.List;
 public interface UserService {
 
     List<UserDTO> listAllUsers();
+
     UserDTO findByUserName(String username);
+
     void save(UserDTO dto);
+
     UserDTO update(UserDTO dto);
+
     void delete(String username);
+
+    void deleteByUserName(String username);
 
 }
