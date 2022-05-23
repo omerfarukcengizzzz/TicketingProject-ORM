8e6db35c134df1ba80c8a493a393ee2b86610ece
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-23 20:53:25 +0100

    Ticketing Project - ORM

--- src/main/java/com/cybertek/entity/Project.java
+++ src/main/java/com/cybertek/entity/Project.java
@@ -1,4 +1,33 @@
 package com.cybertek.entity;
 
-public class Project {
+import com.cybertek.enums.Status;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "projects")
+@Getter
+@Setter
+@NoArgsConstructor
+@Where(clause = "is_deleted = false")
+public class Project extends BaseEntity {
+
+    private String projectName;
+    private String projectCode;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    private String projectDetails;
+    private Status status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "manager_id")
+    private User assignedManager;
+
 }
