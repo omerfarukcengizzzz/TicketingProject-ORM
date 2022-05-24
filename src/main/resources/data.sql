59062cb53333d595965e11cddd1f5cef277647ca
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-24 18:51:44 +0100

    Ticketing Project - ORM

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,5 +5,5 @@ VALUES ('2021-01-05 00:00:00', 1, false, '2021-01-05 00:00:00', 1, 'Admin'),
 
 insert into users(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id, enabled,
                   first_name, gender, last_name, user_name, role_id)
-values ('2021-01-05 00:00:00', 1, false, '2021-01-05 00:00:00', 1, true, 'admin', 'MALE', 'admin', 'admin@admin.com',
-        1);
+values ('2021-01-05 00:00:00', 1, false, '2021-01-05 00:00:00', 1, true, 'admin', 'MALE', 'admin', 'admin@admin.com', 1),
+        ('2021-01-05 00:00:00', 1, false, '2021-01-05 00:00:00', 1, true, 'Omer Faruk', 'MALE', 'CENGIZ', 'omer@gmail.com', 2);
