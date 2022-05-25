f367f21afee4cab74f83825da790e745e53dc893
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-25 23:38:02 +0100

    Ticketing Project - ORM

---
+++ src/main/java/com/cybertek/implementation/TaskServiceImpl.java
@@ -0,0 +1,36 @@
+package com.cybertek.implementation;
+
+import com.cybertek.dto.TaskDTO;
+import com.cybertek.entity.Task;
+import com.cybertek.service.TaskService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+    @Override
+    public TaskDTO findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<TaskDTO> listAllTasks() {
+        return null;
+    }
+
+    @Override
+    public Task save(TaskDTO taskDTO) {
+        return null;
+    }
+
+    @Override
+    public void update(TaskDTO taskDTO) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
