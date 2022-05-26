77f7c520513e8373be20fb2252103341f381dd9d
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-27 00:02:20 +0100

    Ticketing Project - ORM

--- src/main/java/com/cybertek/repository/TaskRepository.java
+++ src/main/java/com/cybertek/repository/TaskRepository.java
@@ -1,9 +1,12 @@
 package com.cybertek.repository;
 
+import com.cybertek.entity.Project;
 import com.cybertek.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
@@ -14,5 +17,7 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     @Query(value = "select count(*) from tasks t join projects p on t.project_id = p.id where p.project_code = ?1 and t.status != 'COMPLETED'",
             nativeQuery = true)
     Integer totalNonCompletedTasks(String projectCode);
+
+    List<Task> findAllByProjectId(Project project);
 
 }
