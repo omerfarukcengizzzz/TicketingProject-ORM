0f6c38b778bbbe10cc4b71b5a3149216224e013d
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-05-26 16:05:17 +0100

    Ticketing Project - ORM

--- src/main/java/com/cybertek/repository/ProjectRepository.java
+++ src/main/java/com/cybertek/repository/ProjectRepository.java
@@ -12,4 +12,6 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     Project findByProjectCode(String code);
 
+    List<Project> findByAssignedManager(User manager);
+
 }
