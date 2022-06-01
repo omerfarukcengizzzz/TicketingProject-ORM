7e18df6f3346f17aba33dc3fe825fe733befa5e1
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-06-01 11:55:07 +0100

    Ticketing Project - ORM

--- src/main/java/com/cybertek/repository/TaskRepository.java
+++ src/main/java/com/cybertek/repository/TaskRepository.java
@@ -26,4 +26,6 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
 
     List<Task> findAllByProjectAssignedManager(User manager);
 
+    List<Task> findAllByAssignedEmployee(User employee);
+
 }
